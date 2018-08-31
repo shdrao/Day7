e6cb08b8c0f38c6edb2401027c48c367d2f742ba
shdrao <shdrao@IN-MUM-LDVM038.corp.capgemini.com>
2018-08-31 09:08:43 -0400

    2

--- src/com/capgemini/day7/WordCount.java
+++ src/com/capgemini/day7/WordCount.java
@@ -9,34 +9,18 @@ public class WordCount {
 
 	public static int count(String string) throws IOException {
 
-//		File f1 = new File(string);
-//		try (FileReader fr = new FileReader(f1); BufferedReader br = new BufferedReader(fr);) {
-//			String line = "", str = "";
-//			int a = 0;
-//			int b = 0;
-//			while ((line = br.readLine()) != null) {
-//				str += line + " ";
-//				b++;
-//				// TODO Auto-generated method stub
-//				
-//			}
-//			return b;
-//		}
-		
-		int count=0;
-		
+		int count = 0;
+
 		try (FileReader fr = new FileReader(string); BufferedReader br = new BufferedReader(fr);) {
-			
+
 			String line;
 			String[] stringArray;
-			while((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				stringArray = line.split(" ");
 				count = count + stringArray.length;
 			}
 		}
 		return count;
-		
-		
-		
+
 	}
 }
